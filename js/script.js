7cf2f83fd9303c8c56a439ac9d52492f730711a6
alifmutaram <alifmutaram22@gmail.com>
2023-08-15 20:30:36 +0700

    add feat : rating component

--- js/script.js
+++ js/script.js
@@ -1,4 +1,23 @@
-function handleBtn() {
-  const box = document.getElementById("rating-1").value;
-  console.log({ box });
-}
+const MainContainer = document.querySelector("main");
+const submit = document.querySelector(".submit");
+const sectionThank = document.querySelector("section");
+const BtnRating = document.querySelectorAll(".btn-rating");
+const containerRtg = document.getElementById("rating");
+const button = document.getElementsByClassName(".btn-rating");
+
+submit.addEventListener("click", () => {
+  MainContainer.classList.add("hidden");
+  sectionThank.classList.remove("hidden");
+});
+
+BtnRating.forEach((rating) => {
+  rating.addEventListener("click", () => {
+    containerRtg.innerHTML = rating.innerHTML;
+  });
+});
+BtnRating.forEach((Rtg) => {
+  Rtg.addEventListener("click", (e) => {
+    e.preventDefault();
+    e.target.style.backgroundColor = "orange";
+  });
+});
